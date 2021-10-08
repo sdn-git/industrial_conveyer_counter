88b6e37cead709a985e653c3e4270917ab4894aa
Dmitrii Solomitckii <77998105+sdn-git@users.noreply.github.com>
2021-10-09 00:44:31 +0300

    Update MathOperations.asm

--- MathOperations.asm
+++ MathOperations.asm
@@ -1,69 +1,58 @@
 MathOperations:
 	PUSH_ALL
-	clr tmp
-Divide:				;Накопленную сумму делим сдвигом на количество выборок, 
-  	inc tmp			;  которое должно быть кратно степени 2.
-	lsr R_ConstCurrent3
-	ror R_ConstCurrent2
-	cpi tmp,C_Shift
-	brne Divide
+	clr 	tmp
 	
-
-
-	MOV R_ConstAverageL,R_ConstCurrent2
+Divide:				
+  	inc 	tmp			
+	lsr 	R_ConstCurrent3
+	ror 	R_ConstCurrent2
+	cpi 	tmp,C_Shift
+	brne 	Divide
+	MOV 	R_ConstAverageL,R_ConstCurrent2
 	MOV	R_ConstAverageH,R_ConstCurrent3
-	clr R_ConstCurrent1
-	clr R_ConstCurrent2
-	clr R_ConstCurrent3	
-	;Только что полученный результат= сумме выборок /SampleCount
-/*
-	add R_ConstAverageL,R_ConstCurrent2	;Усредняем предыдущий и только что полученный результат.
-	adc R_ConstAverageH,R_ConstCurrent3	
-	lsr R_ConstAverageH			
-	ror R_ConstAverageL			
-*/
-						;Вычисляем полосу допуска детектора.
-
-	mov XL,R_ConstAverageL		
-	mov XH,R_ConstAverageH
-
+	clr 	R_ConstCurrent1
+	clr 	R_ConstCurrent2
+	clr 	R_ConstCurrent3	
+	add 	R_ConstAverageL,R_ConstCurrent2	
+	adc 	R_ConstAverageH,R_ConstCurrent3	
+	lsr 	R_ConstAverageH			
+	ror 	R_ConstAverageL			
+	mov 	XL,R_ConstAverageL		
+	mov 	XH,R_ConstAverageH
 	mov	tmp,XL
 	mov	tmp2,XH
-	lsr	tmp2	;Delta=Average/4
+	lsr	tmp2	
 	ror	tmp
-	lsr tmp2
+	lsr 	tmp2
 	ror	tmp	
-;	lsr tmp2
-;	ror	tmp	
-		;Теперь в TMP имеем 8-битный Average-уровень
+	lsr 	tmp2
+	ror	tmp	
 	cpi	tmp,C_Delta
 	brcc	COMP_DeltaMin
-	ldi	tmp,C_Delta	;Если динамическая дельта слишком мала (<C_Delta), то вместо нее
-	clr	tmp2		;   используем константу C_Delta
-	;
-COMP_DeltaMin:
-	sub XL,tmp			;Нижняя граница будет = средний уровень-C_Delta
-	sbc	XH,tmp2
-	;
+	ldi	tmp,C_Delta	
+	clr	tmp2		
 
-	;пока не определим границы,tmp,tmp2 не портим !!!
-	mov R_DeltaMinL,XL		
-	mov R_DeltaMinH,XH
-	brcc COMP_DeltaMax		;Если нижняя границы - ниже "0", то пусть она будет "0"
+COMP_DeltaMin:
+	sub 	XL,tmp			
+	sbc	XH,tmp2
+	mov 	R_DeltaMinL,XL		
+	mov 	R_DeltaMinH,XH
+	brcc 	COMP_DeltaMax		
 	clr	R_DeltaMinL
 	clr	R_DeltaMinH
+	
 COMP_DeltaMax:
-	mov XL,R_ConstAverageL		
-	mov XH,R_ConstAverageH
-	add XL,tmp			;Верхняя граница будет = средний уровень+C_Delta
-	adc XH,tmp2
-	;
-	mov R_DeltaMaxL,XL
-	mov R_DeltaMaxH,XH
-	brcc AfterCompDelta		;Если нижняя границы - выше 0xFFFF, то пусть она будет 0xFFFF.
+	mov 	XL,R_ConstAverageL		
+	mov 	XH,R_ConstAverageH
+	add 	XL,tmp			
+	adc 	XH,tmp2
+	mov 	R_DeltaMaxL,XL
+	mov 	R_DeltaMaxH,XH
+	brcc 	AfterCompDelta		
 	ser	TMP
-	mov R_DeltaMaxL,TMP
-	mov R_DeltaMaxH,TMP
+	mov 	R_DeltaMaxL,TMP
+	mov 	R_DeltaMaxH,TMP
+	
 AfterCompDelta:
 	POP_ALL
 	ret	
