18a731c22ce1c14848fdf993a5cd08bfa3e871d5
Dmitrii Solomitckii <77998105+sdn-git@users.noreply.github.com>
2021-10-09 00:47:24 +0300

    Update PortsTimersInit.asm

--- PortsTimersInit.asm
+++ PortsTimersInit.asm
@@ -1,78 +1,51 @@
 PortsTimersInit:
- 
-  ;Инициализаци портов
-  ;________________________________
-	ldi tmp,0b00000000  ;вход
-	out DDRC,tmp
-	ldi tmp,0b11111111
-	out PORTC,tmp
+	ldi 	tmp,0b00000000  
+	out 	DDRC,tmp
+	ldi 	tmp,0b11111111
+	out 	PORTC,tmp
 
-	ldi tmp,0b11111111	;выход
-	out DDRB,tmp
-	ldi tmp,0b11111111
-	out PORTB,tmp
+	ldi 	tmp,0b11111111	
+	out 	DDRB,tmp
+	ldi 	tmp,0b11111111
+	out 	PORTB,tmp
 
-	ldi tmp,0b11111111	;выход
-	out DDRD,tmp
-	ldi tmp,0b11111111
-	out PORTD,tmp
+	ldi 	tmp,0b11111111	
+	out 	DDRD,tmp
+	ldi 	tmp,0b11111111
+	out 	PORTD,tmp
 
-	cbi PORTB,Const ;при включении конвейер должен стоять на месте.
-
-
-   ;Инициализация собаки
-   ;_________________________________
+	cbi 	PORTB,Const 
 	ldi	tmp,6 			
-   ;out	WDTCR,tmp 		
+   	out	WDTCR,tmp 		
 	wdr
-;	cli          
- 
- 
-   ;Инициализация таймеров
-   ;_________________________________
+	cli          
 
-	ldi	tmp,0b01011100		;Load constant in registr tmp (r16)
-	out	TIMSK,tmp		;Allow interrupt processing
-	ldi	tmp,0b00000001			;Load constant in registr tmp (r16).Constant before is deleting<!!>
-	out	TCCR0,tmp		;T0 frequency equal signal microcontroller
+	ldi	tmp,0b01011100		;T0
+	out	TIMSK,tmp		
+	ldi	tmp,0b00000001		
+	out	TCCR0,tmp		
 	wdr
-	;
 
-   ;Инициализация таймера Т1
-   ;________________________________
-
-;	ldi	tmp,0b00000001
-;	out	TCCR1B,tmp
-	
-   ;Инициализация таймера Т2
-   ;________________________________	
+	ldi	tmp,0b00000001		;T1
+	out	TCCR1B,tmp
 		
-   ldi	tmp,0b00000100		
-   out	TCCR2,tmp	
+   	ldi	tmp,0b00000100		;T2
+  	out	TCCR2,tmp	
 
-   ;Инициализация АЦП
-   ;________________________________
+	ldi 	tmp,0b01000000 		;ADC
+	out 	ADMUX,tmp		
+	ldi 	tmp,0b11101111	
+	out 	ADCSR,tmp
+	ldi 	tmp,0b00010000
+   	out 	SFIOR,tmp
 
-	ldi tmp,0b01000000 	;Опорное напряжение равно напряжению питания.
-	out ADMUX,tmp		;Сначала установим № канала, затем запустим АЦП
-				; !! данные по первой выборке попадут не в свой канал (из-за особенности построения 
-				;  программы переключения каналов АЦП.
-	ldi tmp,0b11101111	;AЦП-включено, преобразование-выключено, режим-непрерывный,нет прерывания компаратора,
-				;прерывание компаратора запрещено,коэффициент деления 32
-	out ADCSR,tmp
-	ldi tmp,0b00010000
-   	out SFIOR,tmp
-
-
-	;Инициализация UART
-	;_______________________________
-	ldi	tmp,0b00011000
-	out UCSRB,tmp
-	ldi tmp, low(BaudRate_constant)
-	ldi xtmp,high(BaudRate_constant)
+	ldi	tmp,0b00011000		;UART
+	out 	UCSRB,tmp
+	ldi 	tmp, low(BaudRate_constant)
+	ldi 	xtmp,high(BaudRate_constant)
 	out	UBRRL,tmp
 	out	UBRRH,xtmp
-	ldi tmp,0b10001111
-	out UCSRC,tmp
+	ldi 	tmp,0b10001111
+	out 	UCSRC,tmp
 	ret
 	
